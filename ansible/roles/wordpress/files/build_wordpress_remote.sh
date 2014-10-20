7660012ccd5882119d1137db511a54b70147910c
Mark Breedlove <mb@dp.la>
2014-10-20 15:05:29 -0400

    Fix wordpress deployment
    Among other things, the wrong Wordpress repositories were being
    checked out.  This changes the deployment to use a single
    repository, the one that's really used for ongoing changes.

--- ansible/roles/wordpress/files/build_wordpress_remote.sh
+++ ansible/roles/wordpress/files/build_wordpress_remote.sh
@@ -2,53 +2,23 @@
 
 # Runs as 'dpla' user
 
-plugins_branch=${1:-master}
+branch_or_tag=${1:-master}
 rsync=/usr/bin/rsync
 
 eval `ssh-agent`
 ssh-add $HOME/git_private_key
 
-# Check out wordpress plugins and transfer into build directory.
-cd $HOME
-if [ -d "$HOME/wordpress-plugins" ]; then
-    cd $HOME/wordpress-plugins
-    git checkout $plugins_branch || exit 1
-    git pull || exit 1
-else
-    git clone git@github.com:dpla/wordpress-plugins.git wordpress-plugins \
-        || exit 1
-    cd $HOME/wordpress-plugins
-    git checkout $branch || exit 1
-fi
-$rsync -ruptoglv --delete --checksum \
-    --exclude '.git' \
-    $HOME/wordpress-plugins/ $HOME/wordpress/wp-content/plugins
-
-# Check out our theme's dependency theme, "egesto", and transfer
-# it into the build directory.
-cd $HOME
-if [ -d "$HOME/frontend-wp-egesto" ]; then
-    cd $HOME/frontend-wp-egesto
-    git pull || exit 1
-else
-    git clone git@github.com:dpla/frontend-wp-egesto.git frontend-wp-egesto \
-        || exit 1
-fi
-$rsync -ruptoglv --delete --checksum \
-    --exclude '.git' \
-    $HOME/frontend-wp-egesto/ $HOME/wordpress/wp-content/themes/egesto
-
 # Check out theme and transfer into build directory.
 # So far, this only has one branch, master.
-cd $HOME
-if [ -d "$HOME/wordpress-theme" ]; then
-    cd $HOME/wordpress-theme
+if [ -d "$HOME/wordpress/.git" ]; then
+    cd $HOME/wordpress
+    git checkout $branch_or_tag
     git pull || exit 1
 else
-    git clone git@github.com:dpla/frontend-wp-theme.git wordpress-theme \
+    rm -rf $HOME/wordpress
+    cd $HOME
+    git clone git@github.com:dpla/frontend-wp.git wordpress \
         || exit 1
+    cd $HOME/wordpress
+    git checkout $branch_or_tag
 fi
-$rsync -ruptoglv --delete --checksum \
-    --exclude '.git' \
-    $HOME/wordpress-theme/ $HOME/wordpress/wp-content/themes/berkman_custom_dpla
-
--- ansible/roles/wordpress/files/sync_to_siteroot.sh
+++ ansible/roles/wordpress/files/sync_to_siteroot.sh
@@ -4,4 +4,7 @@
 rsync=/usr/bin/rsync
 
 $rsync -ruptolg --checksum --delete --delay-updates \
+    --exclude 'wp-content/uploads' \
+    --exclude 'wp-config.php' \
+    --exclude '.git' \
     /home/dpla/wordpress/ /srv/www/wordpress

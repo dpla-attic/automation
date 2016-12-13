f89b879ab8d050b90a554210f3abb0d0b71ca188
Mark Breedlove <mb@markbreedlove.com>
2016-12-13 11:13:13 -0500

    Fix deployment gem issues (#218)
    * Fix various deployments to honor our new strategy of using
      Gemfile.lock as it was intended.
    * Provide clean_* variables for more roles, to allow build directories
      to be cleared.
    * Update README.md with advice on using the clean_* variables. Also
      remove some  information that is misleading because it is incomplete.
      It should be covered elsewhere.
    
    The changes here are to fix problems with Ruby gems being upgraded out
    from under us and breaking deployments. To keep ourselves from having to
    diligently pin random gems in our Gemfiles, we have decided to use
    Gemfile.lock files for their intended purpose. We must therefore stop
    removing Gemfile.lock before we do a `bundle install`.
    
    The heidrun and frontend applications are exceptions. (Heidrun is
    deployed in the ingestion_app role.)  These applications have
    complicating factors that prevent us from using a Gemfile.lock at the
    moment.  Frontend has some optionally-included assets in a private
    repository and ingestion_app, and heidrun's development configuration
    requires a little more thought related to local versus repository
    deployments before committing anything.
    
    This changeset assumes that you will deploy current versions of the
    platform, frontend, and primary-source-sets applications, which have
    Gemfile.lock files! If you want to deploy an old version, use the
    relevant clean_ variable on the commandline with ansible-playbook, such
    as `-e 'clean_api=true'`.

--- ansible/roles/pss/files/build_pss.sh
+++ ansible/roles/pss/files/build_pss.sh
@@ -23,7 +23,6 @@ rbenv shell $USE_VERSION
 echo "using version ${USE_VERSION} ..." >> $LOGFILE
 echo "installing bundle ..." >> $LOGFILE
 
-rm -f Gemfile.lock
 bundle install >> $LOGFILE 2>&1
 if [ $? -ne 0 ]; then
     exit 1

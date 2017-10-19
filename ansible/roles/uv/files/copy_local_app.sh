7b0afd2c44d7e91df589494a84ad1b4eb7c3ec81
Mark Breedlove <mb@markbreedlove.com>
2017-10-19 13:11:09 -0400

    Remove uv role (#244)

--- ansible/roles/uv/files/copy_local_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -d /universalviewer ]; then
-    >&2 echo "Local directory /universalviewer does not exist (check Vagrantfile)"
-    exit 1
-fi
-
-rsync -rIptl --delete --checksum --exclude '.git*' \
-    /universalviewer/ /opt/uv/src/universalviewer-local
